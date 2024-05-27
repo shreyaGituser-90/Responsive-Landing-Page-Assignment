2f2ca28775e049b0453cc56d3108037683771f29
Shreya Das <91009362+shreyaGituser-90@users.noreply.github.com>
2024-05-28 00:00:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById("hamburger");
+  const navItems = document.getElementById("nav-items");
+
+  hamburger.addEventListener("click", () => {
+    navItems.classList.toggle("show");
+  });
+});
